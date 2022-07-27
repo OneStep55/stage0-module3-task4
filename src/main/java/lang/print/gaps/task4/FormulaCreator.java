e993a12629d410e11800e6b2fe56e5942abc8761
OneStep55 <vnmcfn@gmail.com>
2022-07-27 21:27:42 +0600

    Complete tasks

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,16 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int nine = 9;
+        char a  = 'a';
+        int two = 2;
+        int five = 5;
+        char b = 'b';
+        int seven = 7;
+        int four = 4;
+
+        System.out.println("(" + nine + "" + a + "^" + two + " - " + five + "" + b + " + " + two + " + " + a +
+                ") * ((" + a + " + " + b + " - " + four + "" + a + "" + b + ") /" + two + ")");
+
     }
 }
